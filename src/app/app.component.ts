4f9f27b717b2d8f40ceb46d7d7bda3930eacefa7
skazure <41524841+skazure@users.noreply.github.com>
2022-07-15 10:10:53 +0200

    Update app.component.ts
    HttpClient response added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FE-Angular-App1';
+  
+  response = "Oops!!! Looks like Backend API is down. Please ensure BE API1 is running !!!";
+  beApp1response = "Oops!!! Looks like Backend API is down. Please ensure BE API1 is running !!!";
+
+  constructor(private http: HttpClient) 
+  { 
+    this.http.get('http://localhost:5001/demo', {responseType: 'text'}).subscribe((response: any) => {
+      console.log(response);      
+	  this.response = response;   
+	});
+
+  this.http.get('http://localhost:5002/demo', {responseType: 'text'}).subscribe((beApp1response: any) => {
+      console.log(beApp1response);      
+	  this.beApp1response = beApp1response;   
+	});
+
+  }
+  
 }
