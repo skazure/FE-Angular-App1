dd37ae5e92e2f11c6ff5ba60d159018f9bb79593
skazure <41524841+skazure@users.noreply.github.com>
2022-08-23 12:13:57 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  bemsurl1: "http://localhost:20851/demo",
+  bemsurl2: "http://localhost:20851/demo"
 };
 
 /*
